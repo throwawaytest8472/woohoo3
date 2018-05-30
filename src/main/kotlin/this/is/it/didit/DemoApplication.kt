e84263098fb921db494fb91567805755df0ad9e4
pivotal <Pivotal Default>
2018-05-30 16:58:00 -0700

    Initial commit

---
+++ src/main/kotlin/this/is/it/didit/DemoApplication.kt
@@ -0,0 +1,11 @@
+package this.is.it.didit
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class DemoApplication
+
+fun main(args: Array<String>) {
+    SpringApplication.run(DemoApplication::class.java, *args)
+}
